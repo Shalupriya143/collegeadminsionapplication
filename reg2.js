7a41415bbd6502e1030afbad8d72eacb9637d6b3
Shalupriya143 <86945052+Shalupriya143@users.noreply.github.com>
2021-07-05 11:13:07 +0530

    Add files via upload

---
+++ reg2.js
@@ -0,0 +1,21 @@
+ 
+  function show1()
+  {
+  document.getElementById('div1').style.display = 'block';
+}
+function show2()
+{
+  document.getElementById('div1').style.display = 'none';
+}
+function display1(){
+  document.getElementById('div2').style.display = 'block';
+}
+function display2(){
+  document.getElementById('div2').style.display = 'none';
+}
+function see1(){
+  document.getElementById('div3').style.display = 'block';
+}
+function see2(){
+  document.getElementById('div3').style.display = 'none';
+}
