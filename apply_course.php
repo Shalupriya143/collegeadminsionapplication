aaa9d20509ba95f6b0b5a84ee36d76a9fddfdced
Shalupriya143 <86945052+Shalupriya143@users.noreply.github.com>
2021-07-06 11:35:21 +0530

    Add files via upload

--- apply_course.php
+++ apply_course.php
@@ -13,22 +13,18 @@ while($res=mysqli_fetch_assoc($selectresult))
 <body>
 	<form action="selectcoursesinsert.php" method="post">
 <?php
-$sql="SELECT courseid FROM displaycoursedetails WHERE studentid='$studentid'";
-$result=mysqli_query($conn,$sql);
-while($res=mysqli_fetch_assoc($result))
-{
-	$courseid=$res['courseid'];
-	$print="SELECT * FROM courses WHERE coursesid != '$courseid'";
-	$printresult=array(mysqli_query($conn,$print));
-}
-	foreach($printresult as $res){
+	$sql="SELECT * FROM courses WHERE coursesid NOT IN (SELECT courseid FROM displaycoursedetails WHERE studentid='$studentid')";
+	$printresult=mysqli_query($conn,$sql);
+	while($res=mysqli_fetch_assoc($printresult))
+	{
 ?>
 <input type="checkbox" name="techno[]" value="<?php echo $res['coursesid'];?>">
-<?php echo $res['coursesname'];
+<?php echo $res['coursesname'];?>
+<br>
+<?php
 }
 ?><br>
-		<button type="submit" name="submit">Submit</button>
-		<a href="usercoursedetailsview.php">View</a>	
+		<button type="submit" name="submit">Submit</button>	
 </form>
 </body>
 </html>
